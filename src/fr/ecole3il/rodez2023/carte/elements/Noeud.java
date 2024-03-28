de564ff9d06429d0c98901b9109ae155ad5189fb
Feral Theo <feralt@3il.fr>
2024-03-28 19:55:59 +0100

    Ajout de commentaire dans la classe Noeud

--- src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -3,25 +3,53 @@ package fr.ecole3il.rodez2023.carte.elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe Noeud représente un nœud générique dans une structure de graphe. Un
+ * nœud possède une valeur de type générique E et une liste de nœuds voisins,
+ * également de type E.
+ *
+ * @param <E> Le type générique des valeurs stockées dans les nœuds.
+ */
 public class Noeud<E> {
-	
+
 	private E valeur;
 	private List<Noeud<E>> voisin;
 
-	public Noeud(E valeur){
+	/**
+	 * Construit un nouvel objet Noeud avec la valeur spécifiée et initialise la
+	 * liste de voisins.
+	 *
+	 * @param valeur La valeur du nœud.
+	 */
+	public Noeud(E valeur) {
 		super();
 		this.valeur = valeur;
 		this.voisin = new ArrayList<>();
 	}
 
+	/**
+	 * Retourne la valeur du nœud.
+	 *
+	 * @return La valeur du nœud.
+	 */
 	public E getValeur() {
 		return valeur;
 	}
-	
-	public List<Noeud<E>> getVoisins(){
+
+	/**
+	 * Retourne la liste des nœuds voisins.
+	 *
+	 * @return La liste des nœuds voisins.
+	 */
+	public List<Noeud<E>> getVoisins() {
 		return voisin;
 	}
-	
+
+	/**
+	 * Ajoute un nœud voisin à la liste des voisins du nœud actuel.
+	 *
+	 * @param voisin Le nœud voisin à ajouter.
+	 */
 	public void ajouterVoisin(Noeud<E> voisin) {
 		this.voisin.add(voisin);
 	}
